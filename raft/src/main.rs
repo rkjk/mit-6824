de3ba46a6774199a7fe8417250599b001620be92
Raghav Kuppan <raghav.kuppan@gatech.edu>
2020-09-13 17:54:52 -0400

    Add test for election timer; move election timer to a separate function

--- raft/src/main.rs
+++ raft/src/main.rs
@@ -1,4 +1,4 @@
-use crossbeam_channel::{unbounded, RecvTimeoutError};
+use crossbeam_channel::{unbounded, Receiver, RecvTimeoutError};
 use jsonrpc_core::types::error;
 use jsonrpc_core::Result;
 use jsonrpc_derive::rpc;
@@ -56,22 +56,13 @@ struct NodeRpc {
 impl NodeRpc {
     fn new() -> NodeRpc {
         NodeRpc {
-            node: Arc::new(RwLock::new(Node {
-                currentTerm: 0,
-                state: State::Follower,
-                votedFor: None,
-                log: Vec::new(),
-            })),
+            node: Arc::new(RwLock::new(Node::new())),
         }
     }
 }
 
-pub fn main() {
-    let (tx, rx) = unbounded();
-    let mut node_rpc = NodeRpc::new();
-    let mut node_clone = Arc::clone(&node_rpc.node);
-
-    std::thread::spawn(move || loop {
+fn election_timer(rx: Receiver<bool>, node_clone: Arc<RwLock<Node>>) {
+    loop {
         let mut rng = rand::thread_rng();
         let timeout = Duration::from_millis(rng.gen_range(150, 300));
         match rx.recv_timeout(timeout) {
@@ -92,19 +83,39 @@ pub fn main() {
                 break;
             }
         }
-    });
+    }
+}
 
-    for _ in 0..10 {
-        println!("Main thread: Send ping");
-        let timeout_reset = tx.send(true);
-        match timeout_reset {
-            Ok(_) => {
-                let mut rng = rand::thread_rng();
-                std::thread::sleep_ms(rng.gen_range(150, 300));
-            }
-            Err(_) => {
-                println!("Main Thread: Cannot communicate with election timeout thread. Exit");
-                break;
+fn main() {
+    println!("Starting Server");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_election_timeout() {
+        let (tx, rx) = unbounded();
+        let mut node_rpc = NodeRpc::new();
+        let mut node_clone = Arc::clone(&node_rpc.node);
+
+        std::thread::spawn(move || {
+            election_timer(rx, node_clone);
+        });
+
+        for _ in 0..10 {
+            println!("Main thread: Send ping");
+            let timeout_reset = tx.send(true);
+            match timeout_reset {
+                Ok(_) => {
+                    let mut rng = rand::thread_rng();
+                    std::thread::sleep_ms(rng.gen_range(150, 300));
+                }
+                Err(_) => {
+                    println!("Main Thread: Cannot communicate with election timeout thread. Exit");
+                    break;
+                }
             }
         }
     }
