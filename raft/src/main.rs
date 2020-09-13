2a4efb7dceae17e8e91ff10cb176688cf92b66df
Raghav Kuppan <raghav.kuppan@gatech.edu>
2020-09-13 18:29:47 -0400

    Added basic RequestVote RPC support

--- raft/src/main.rs
+++ raft/src/main.rs
@@ -1,4 +1,4 @@
-use crossbeam_channel::{unbounded, Receiver, RecvTimeoutError};
+use crossbeam_channel::{unbounded, Receiver, RecvTimeoutError, Sender};
 use jsonrpc_core::types::error;
 use jsonrpc_core::Result;
 use jsonrpc_derive::rpc;
@@ -51,16 +51,54 @@ impl Node {
 
 struct NodeRpc {
     node: Arc<RwLock<Node>>,
+    tx_election_timer: Sender<bool>,
+    id: u64,
 }
 
 impl NodeRpc {
-    fn new() -> NodeRpc {
-        NodeRpc {
+    fn new(id: u64) -> NodeRpc {
+        // Create channel for communication between main thread and election timer thread.
+        // Move rx to the election timer thread and tx to the NodeRpc Object
+        let (tx, rx) = unbounded();
+        let node_rpc = NodeRpc {
             node: Arc::new(RwLock::new(Node::new())),
-        }
+            tx_election_timer: tx,
+            id: id,
+        };
+
+        let node_clone = Arc::clone(&node_rpc.node);
+        std::thread::spawn(move || election_timer(rx, node_clone));
+
+        return node_rpc;
     }
 }
 
+impl Rpc for NodeRpc {
+    /// RequestVote Rpcs are handled here
+    fn request_vote(&self) -> Result<String> {
+        println!("Node {}: Got RequestVote Rpc", self.id);
+        match self.tx_election_timer.send(true) {
+            Ok(_) => {
+                println!("Reset Election timer");
+            }
+            Err(_) => {
+                println!(
+                    "Main Thread: Cannot communicate with election timeout thread. Return Early"
+                );
+                return Err(error::Error {
+                    code: error::ErrorCode::InternalError,
+                    message: "Something went wrong when resetting election timer".to_owned(),
+                    data: None,
+                });
+            }
+        }
+        Ok("Got Vote".to_string())
+    }
+}
+
+/// Election Timer function. Accepts Receiver's side of an unbounded channel and a Arc to the Node Object.
+/// The Timer is reset everytime it receives communication from the main thread. If no communication for a period
+/// of 150-300ms (random), then election is called.
 fn election_timer(rx: Receiver<bool>, node_clone: Arc<RwLock<Node>>) {
     loop {
         let mut rng = rand::thread_rng();
@@ -87,7 +125,17 @@ fn election_timer(rx: Receiver<bool>, node_clone: Arc<RwLock<Node>>) {
 }
 
 fn main() {
-    println!("Starting Server");
+    let mut node_rpc = NodeRpc::new(0);
+
+    let mut io = jsonrpc_core::IoHandler::new();
+    io.extend_with(node_rpc.to_delegate());
+    let server = ServerBuilder::new(io)
+        .threads(1)
+        .start_http(&"127.0.0.1:3030".parse().unwrap())
+        .unwrap();
+    let close_handle = server.close_handle();
+    println!("Node Address: {:?}", server.address());
+    server.wait();
 }
 
 #[cfg(test)]
@@ -97,7 +145,7 @@ mod tests {
     #[test]
     pub fn test_election_timeout() {
         let (tx, rx) = unbounded();
-        let mut node_rpc = NodeRpc::new();
+        let mut node_rpc = NodeRpc::new(0);
         let mut node_clone = Arc::clone(&node_rpc.node);
 
         std::thread::spawn(move || {
