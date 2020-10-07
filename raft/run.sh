5c04ec09980c8abd66a0f05bdc63b97ca09c6ef5
Raghav Kuppan <raghav.kuppan@gatech.edu>
2020-10-06 20:48:59 -0400

    Add options for Debug and Full backtrace debug

--- raft/run.sh
+++ raft/run.sh
@@ -4,14 +4,30 @@ set -euo pipefail
 
 trap 'kill 0' EXIT
 
+DEBUG=${1:-nodebug}
+
 cargo build
 
 # Make Sure `config` has enough entries with unique IP:port - In this case 3 entries, each entry
 # of the form `IP PORT` followed by newline
-for i in {0..2}
-do
-  ./target/debug/raft "$i" > $i.log 2>&1 &
-done
+if [ $DEBUG = "debug" ]
+then
+  for i in {0..2}
+  do
+    RUST_BACKTRACE=1 ./target/debug/raft "$i" > $i.log 2>&1 &
+  done
+elif [ $DEBUG = "full" ]
+then
+  for i in {0..2}
+  do
+    RUST_BACKTRACE=full ./target/debug/raft "$i" > $i.log 2>&1 &
+  done
+else
+  for i in {0..2}
+  do
+    ./target/debug/raft "$i" > $i.log 2>&1 &
+  done
+fi
 
 echo "Started workers"
 
